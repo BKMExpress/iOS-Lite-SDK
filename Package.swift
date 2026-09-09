82aa85f743e32ea3e492750f39486f0f7399e554
abatmaz <Ardagokalp.batmaz@bkm.com.tr>
2026-09-09 14:57:10 +0300

    1.1.2

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+  name: "BKMExpressLiteSDK",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "BKMExpressLiteSDK",
+      targets: ["BKMExpressLiteSDK"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "BKMExpressLiteSDK",
+      url: "https://entegrasyon-repo.bkmexpress.com.tr/repository/swift/iossdk/ios_lite_sdk/1.1.2.zip",
+      checksum: "effc71aea8ece981f58e25abd82e33eda080233e3558f7f9cf3df9f0f81600b0"
+    )
+  ]
+)
